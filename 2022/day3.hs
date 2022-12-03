f9c7d4fabc8dc5b6240bece90acd147624b84e29
Nicolas Porcel <nicolasporcel06@gmail.com>
2022-12-03 17:10:39 +0000

    2022 Day 3 Part 2

--- 2022/day3.hs
+++ 2022/day3.hs
@@ -1,6 +1,6 @@
-import ParseAndRun
 import Data.Char
 import Data.List
+import ParseAndRun
 
 itemPriority :: Char -> Int
 itemPriority item
@@ -16,5 +16,11 @@ duplicateItem items = head $ c1 `intersect` c2
 part1 :: [String] -> Int
 part1 = sum . map (itemPriority . duplicateItem)
 
+part2 :: [String] -> Int
+part2 (elf1 : elf2 : elf3 : rest) = itemPriority groupItem + part2 rest
+  where
+    groupItem = head (elf1 `intersect` elf2 `intersect` elf3)
+part2 _ = 0
+
 main :: IO ()
-main = parseAndRun "inputs/day3" part1 part1
+main = parseAndRun "inputs/day3" part1 part2
