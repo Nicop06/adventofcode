d077fd11d576604e94809e01ab7ca9a71cced2df
Nicolas Porcel <nicolasporcel06@gmail.com>
2022-12-06 02:26:35 +0000

    Finish Day5 using Parsec

--- 2022/ParseAndRun.hs
+++ 2022/ParseAndRun.hs
@@ -1,12 +1,14 @@
 module ParseAndRun
-(
-    parseAndRun
-)
+  ( parseAndRun,
+    parseAndSolve,
+  )
 where
 
 import System.Environment
+import Text.Parsec
+import Text.Parsec.String
 
-parseAndRun :: Show a => String -> ([String] -> a) -> ([String] -> a) -> IO ()
+parseAndRun :: Show a => FilePath -> ([String] -> a) -> ([String] -> a) -> IO ()
 parseAndRun input part1 part2 = do
   (part : _) <- getArgs
   contents <- readFile input
@@ -14,3 +16,13 @@ parseAndRun input part1 part2 = do
     "part1" -> print . part1 $ lines contents
     "part2" -> print . part2 $ lines contents
     _ -> return ()
+
+parseAndSolve :: Show a => FilePath -> Parser a -> Parser a -> IO ()
+parseAndSolve input part1 part2 = do
+  (part : _) <- getArgs
+  case part of
+    "part1" -> solve part1
+    "part2" -> solve part2
+    _ -> return ()
+  where
+    solve parser = parseFromFile parser input >>= either print print
---
+++ 2022/day5.hs
@@ -0,0 +1,68 @@
+import Control.Monad
+import Data.List (transpose)
+import Data.Maybe (catMaybes)
+import Text.Parsec
+import Text.Parsec.String
+import ParseAndRun
+
+-- Data
+
+type Crate = Char
+
+type Stack = [Crate]
+
+data Move = Move {numCrates :: Int, from :: Int, to :: Int} deriving (Show)
+
+-- Parser
+
+integer :: Parser Int
+integer = read <$> many1 digit
+
+crate :: Parser Crate
+crate = between (char '[') (char ']') anyChar
+
+createOrEmpty :: Parser (Maybe Crate)
+createOrEmpty = try (Nothing <$ string "   ") <|> (Just <$> crate)
+
+crateLine :: Parser [Maybe Crate]
+crateLine = sepBy createOrEmpty (char ' ') <* newline
+
+crateStacks :: Parser [Stack]
+crateStacks = map catMaybes . transpose <$> many1 crateLine
+
+stackNum :: Parser ()
+stackNum = void $ many (digit <|> char ' ') <* newline
+
+moveLine :: Parser Move
+moveLine = Move <$> parseNumCrates <*> parseFrom <*> parseTo
+  where
+    parseNumCrates = string "move " *> integer
+    parseFrom = string " from " *> integer
+    parseTo = string " to " *> integer
+
+parseInput :: Parser ([Stack], [Move])
+parseInput = (,) <$> (crateStacks <* stackNum <* newline) <*> sepEndBy moveLine newline <* eof
+
+-- Solver
+
+runMove :: (Stack -> Stack) -> [Stack] -> Move -> [Stack]
+runMove f stacks (Move numCrates from to) = 
+  let fromStack = stacks !! (from - 1)
+      toStack = stacks !! (to - 1)
+      createsToMove = take numCrates fromStack
+      fromStackAfter = drop numCrates fromStack
+      toStackAfter = f createsToMove ++ toStack
+      stackAfter = take (from - 1) stacks ++ fromStackAfter : drop from stacks
+   in take (to - 1) stackAfter ++ toStackAfter : drop to stackAfter
+
+solve :: (Stack -> Stack) -> ([Stack], [Move]) -> String
+solve f (stacks, moves) = map head $ foldl (runMove f) stacks moves
+
+part1 :: Parser String
+part1 = solve reverse <$> parseInput
+
+part2 :: Parser String
+part2 = solve id <$> parseInput
+
+main :: IO ()
+main = parseAndSolve "inputs/day5" part1 part2
