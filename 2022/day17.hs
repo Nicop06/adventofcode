5732b3dbc00f140a75408821d5847201bbc4ec57
Nicolas Porcel <nicolasporcel06@gmail.com>
2022-12-18 02:08:39 +0000

    Day 17 Part 1

--- 2022/day17.hs
+++ 2022/day17.hs
@@ -1,5 +1,8 @@
+import Control.Applicative (ZipList (..), getZipList)
+import Data.Bifunctor (first, second)
 import Data.Either (fromRight)
-import Data.List (transpose)
+import Data.List
+import Data.Maybe (fromMaybe)
 import ParseAndRun
 import Text.Parsec
 import Text.Parsec.String
@@ -8,93 +11,119 @@ import Text.Parsec.String
 
 data Jet = JetRight | JetLeft deriving (Show)
 
-data Rock = Hole | Rock deriving (Show, Eq)
+data Rock = Hole | Rock deriving (Show, Eq, Ord)
 
 type RockShape = [[Rock]]
 
-type CaveHeight = [Int]
+type Cave = [[Rock]]
 
 type Position = (Int, Int)
 
 type FallingRock = (Position, RockShape)
 
-data CaveState = CaveState {nextShapes :: [RockShape], nextJets :: [Jet], caveHeight :: CaveHeight, fallingRock :: FallingRock} deriving (Show)
+data CaveState = CaveState {nextShapes :: [RockShape], nextJets :: [Jet], cave :: Cave, fallingRock :: FallingRock} deriving (Show)
 
 caveWidth = 7 :: Int
 
-initialCave = replicate caveWidth 0 :: CaveHeight
+initialCave = [replicate caveWidth Rock]
 
 -- Helpers
 
-initState :: [RockShape] -> [Jet] -> CaveState
-initState shapes jets = CaveState allShapes allJets initialCave firstFallingRock
+initState :: Int -> [RockShape] -> [Jet] -> CaveState
+initState numFalling shapes jets =
+  CaveState allShapes allJets initialCave firstFallingRock
   where
-    firstFallingRock = newFallingRock initialCave (head shapes)
+    firstFallingRock = newFallingRock (head shapes)
     forever = concat . repeat
-    allShapes = tail . forever $ shapes
-    allJets = forever jets
+    allShapes = take numFalling . tail . concat . repeat $ shapes
+    allJets = concat . repeat $ jets
 
-newFallingRock :: CaveHeight -> RockShape -> FallingRock
-newFallingRock c r = ((2, caveRockHeight c + 3), r)
+newFallingRock :: RockShape -> FallingRock
+newFallingRock shape = ((2, -3), shape)
 
-caveRockHeight :: CaveHeight -> Int
-caveRockHeight = maximum
+intersectsWithCave :: Cave -> FallingRock -> Bool
+intersectsWithCave _ ((_, _), []) = False
+intersectsWithCave cave rock@((_, y), _) =
+  let (_, shapesToIntersect) = shapesAboveAndBelowCave rock
+      (caveRowsToIntersect, _) = splitAt y cave
+      bothRocks x y = (x, y) == (Rock, Rock)
+   in any or $ zipWith bothRocks <$> ZipList shapesToIntersect <*> ZipList caveRowsToIntersect
 
-pushRock :: Jet -> FallingRock -> FallingRock
-pushRock JetLeft fr@((x, y), r)
-  | x > 0 = ((x - 1, y), r)
-  | otherwise = fr
-pushRock JetRight fr@((x, y), r)
-  | (x + length (head r)) < caveWidth = ((x + 1, y), r)
-  | otherwise = fr
+mergeRockWithCave :: Cave -> FallingRock -> Cave
+mergeRockWithCave cave fr@((x, y), shape) =
+  let (rocksToAdd, shapeToMerge) = shapesAboveAndBelowCave fr
+      (caveRowsToMerge, rs) = splitAt y cave
+      mergedRows = getZipList $ zipWith max <$> ZipList caveRowsToMerge <*> ZipList shapeToMerge
+   in rocksToAdd ++ mergedRows ++ rs
+
+shapesAboveAndBelowCave :: FallingRock -> (RockShape, RockShape)
+shapesAboveAndBelowCave ((x, y), shape) = splitAt (length shape - y) (padCol y $ map (padRow x) shape)
+
+padRow :: Int -> [Rock] -> [Rock]
+padRow offset rocks = take 7 $ rocksLeft ++ rocks ++ repeat Hole
+  where
+    rocksLeft = replicate offset Hole
+
+padCol :: Int -> RockShape -> RockShape
+padCol offset rocks = replicate (offset - length rocks) (replicate caveWidth Hole) ++ rocks
+
+caveRowAt :: Cave -> Int -> [Rock]
+caveRowAt cave i =
+  let (_, rs) = splitAt i cave
+   in case rs of
+        row : _ -> row
+        [] -> replicate caveWidth Hole
+
+pushRock :: Jet -> CaveState -> CaveState
+pushRock JetLeft = fromMaybe <$> id <*> updateRockPosition (first (subtract 1))
+pushRock JetRight = fromMaybe <$> id <*> updateRockPosition (first (+ 1))
+
+updateRockPosition :: (Position -> Position) -> CaveState -> Maybe CaveState
+updateRockPosition f state@(CaveState shapes jets cave r@(p, rs)) =
+  let (x, y) = f p
+      newRock = ((x, y), rs)
+   in if x < 0 || x + (length . head $ rs) > caveWidth || intersectsWithCave cave newRock
+        then Nothing
+        else Just $ state {fallingRock = newRock}
 
 rockFalls :: CaveState -> CaveState
-rockFalls (CaveState shapes jets c r@((x, y), rs)) =
-  let rockHeight = fallingRockBottom r
-      hasCollidingRock = or $ zipWith (==) c rockHeight
-   in if hasCollidingRock
-        then CaveState (tail shapes) jets (zipWith (+) (fallingRockTop r) c) (newFallingRock c (head shapes))
-        else CaveState shapes jets c ((x, y - 1), rs)
+rockFalls state@(CaveState shapes jets cave rock) =
+  case updateRockPosition (second (+ 1)) state of
+    Just newState -> newState
+    Nothing ->
+      let newCave = mergeRockWithCave cave rock
+       in state
+            { cave = newCave,
+              fallingRock = newFallingRock (head shapes),
+              nextShapes = tail shapes
+            }
+
+caveRockHeight :: Cave -> Int
+caveRockHeight = subtract 1 . length
 
 updateCaveState :: CaveState -> CaveState
-updateCaveState (CaveState shapes jets c r) =
-  let newRock = pushRock (head jets) r
-   in rockFalls (CaveState shapes (tail jets) c newRock)
+updateCaveState state@(CaveState shapes jets c r) =
+  let newState = pushRock (head jets) state
+   in rockFalls $ newState {nextJets = tail jets}
 
-fallingRockBottom :: FallingRock -> [Int]
-fallingRockBottom ((x, y), r) = concat [beforeRock, rockHeight, afterRock]
+runSimulation :: CaveState -> [CaveState]
+runSimulation state = case updateCaveState state of
+  newState@(CaveState [] _ _ _) -> [newState]
+  newState -> newState : runSimulation newState
+
+heightAfterSimulation :: CaveState -> Int
+heightAfterSimulation = caveRockHeight . cave . last . runSimulation
+
+showCave :: Cave -> [String]
+showCave = map (map caveToChar)
   where
-    beforeRock = replicate x maxBound
-    afterRock = replicate (caveWidth - x - length (head r)) maxBound
-    rockHeight = map (+ y) $ rockShapeBottom r
+    caveToChar Hole = '.'
+    caveToChar Rock = '#'
 
-fallingRockTop :: FallingRock -> [Int]
-fallingRockTop ((x, y), r) = concat [beforeRock, rockHeight, afterRock]
-  where
-    beforeRock = replicate x 0
-    afterRock = replicate (caveWidth - x - length (head r)) 0
-    rockHeight = rockShapeTop r
-
-rockShapeBottom :: RockShape -> [Int]
-rockShapeBottom = rockShapeHeight . transpose . reverse
-
-rockShapeTop :: RockShape -> [Int]
-rockShapeTop rock =
-  let totalHeight = length rock
-      rockHeight = rockShapeHeight $ transpose rock
-   in map (totalHeight -) rockHeight
-
-rockShapeHeight :: RockShape -> [Int]
-rockShapeHeight [] = []
-rockShapeHeight (r : rs) = colHeight r : rockShapeHeight rs
-  where
-    colHeight = fst . head . filter ((== Rock) . snd) . zip [0 ..]
-
-runSimulation :: Int -> CaveState -> CaveState
-runSimulation numSteps state = foldl (flip . const $ updateCaveState) state [1 .. numSteps]
-
-heightAfterSimulation :: Int -> CaveState -> Int
-heightAfterSimulation = (fmap . fmap) (maximum . caveHeight) runSimulation
+showState :: CaveState -> [String]
+showState (CaveState _ jets cave fs@((x, y), shapes))
+  | y <= 0 = showCave (map (padRow x) shapes ++ replicate (-y) (replicate caveWidth Hole) ++ cave) ++ [show (head jets)]
+  | otherwise = showCave (mergeRockWithCave cave fs) ++ [show (head jets)]
 
 -- Parser
 
@@ -116,13 +145,15 @@ parseJetPattern = many1 (jetLeft <|> jetRight) <* newline <* eof
     jetRight = JetRight <$ char '>'
 
 part1 :: [RockShape] -> Parser Int
-part1 rocks = heightAfterSimulation 2022 . initState rocks <$> parseJetPattern
+part1 rocks = heightAfterSimulation . initState 2022 rocks <$> parseJetPattern
 
-part2 = return 0
+part2 :: [RockShape] -> Parser Int
+part2 rocks = heightAfterSimulation .  initState 1000000000000 rocks <$> parseJetPattern
+
+showAllStates :: [CaveState] -> [String]
+showAllStates = intercalate ["-------"] . map showState
 
 main :: IO ()
 main = do
   rocks <- parseAllRocks
-  parseAndSolve "/tmp/testinput" (part1 rocks) part2
-
--- parseAndSolve "inputs/day17" (part1 rocks) part2
+  parseAndSolve "inputs/day17" (part1 rocks) (part2 rocks)
