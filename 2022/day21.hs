6363f7b52615f64ad105b768a95da6003b2c7cce
Nicolas Porcel <nicolasporcel06@gmail.com>
2022-12-23 23:36:32 +0100

    Day 21 Part 2

--- 2022/day21.hs
+++ 2022/day21.hs
@@ -8,9 +8,13 @@ import Text.Parsec.String
 
 type MonkeyId = String
 
-data MonkeyYelling = Number Int | Operation Op MonkeyId MonkeyId deriving Show
+data MonkeyYelling = Number Int | Var | Operation Op MonkeyId MonkeyId deriving Show
 
 data Op = Add | Sub | Mult | Div deriving Show
+
+data Result = Result Int | Equation (Int -> Int)
+
+data OpSide = LeftOp | RightOp
 
 type Monkeys = M.Map MonkeyId MonkeyYelling
 
@@ -22,16 +26,47 @@ computeOperation Sub = (-)
 computeOperation Div = (div)
 computeOperation Mult = (*)
 
-computeYelling :: Monkeys -> MonkeyYelling -> Int
-computeYelling _ (Number n) = n
-computeYelling monkeys (Operation op m1 m2) = computeOperation op r1 r2
+computeReversedOperation :: OpSide -> Op -> Int -> Int -> Int
+computeReversedOperation _ Add = flip (-)
+computeReversedOperation LeftOp Sub = (-)
+computeReversedOperation RightOp Sub = (+)
+computeReversedOperation _ Mult = flip div
+computeReversedOperation LeftOp Div = (div)
+computeReversedOperation RightOp Div = (*)
+
+computeYelling :: Monkeys -> MonkeyYelling -> Result
+computeYelling _ (Number n) = Result n
+computeYelling _ Var = Equation id
+computeYelling monkeys (Operation op m1 m2) = operationOrEquation op r1 r2
     where Just y1 = M.lookup m1 monkeys
           Just y2 = M.lookup m2 monkeys
           r1 = computeYelling monkeys y1
           r2 = computeYelling monkeys y2
 
+operationOrEquation :: Op -> Result -> Result -> Result
+operationOrEquation op (Equation e1) (Equation e2) = error "Cannot combine two equations"
+operationOrEquation op (Result r1) (Result r2) = Result $ computeOperation op r1 r2
+operationOrEquation op (Result r) (Equation eq) = Equation (eq . (computeReversedOperation LeftOp op r))
+operationOrEquation op (Equation eq) (Result r) = Equation (eq . (computeReversedOperation RightOp op r))
+
 computeRoot :: Monkeys -> Int
-computeRoot monkeys = let Just m = M.lookup "root" monkeys in computeYelling monkeys m
+computeRoot monkeys =
+    let Just m = M.lookup "root" monkeys
+        Result r = computeYelling monkeys m
+        in r
+
+computeHumanInput :: Monkeys -> Int
+computeHumanInput monkeys =
+    let monkeys' = M.insert "humn" Var monkeys
+        Just (Operation op left right) = M.lookup "root" monkeys'
+        Just ll = M.lookup left monkeys'
+        Just lr = M.lookup right monkeys'
+    in computeEquation (computeYelling monkeys' ll) (computeYelling monkeys' lr)
+
+computeEquation :: Result -> Result -> Int
+computeEquation (Result n) (Equation eq) = eq $ n
+computeEquation (Equation eq) (Result n) = eq $ n
+computeEquation _ _ = error "Need one equation and one result"
 
 -- Parser
 
@@ -68,7 +103,8 @@ parseInput = M.fromList <$> parseMonkey `sepEndBy1` newline <* eof
 part1 :: Parser Int
 part1 = computeRoot <$> parseInput
 
-part2 = return 0
+part2 :: Parser Int
+part2 = computeHumanInput <$> parseInput
 
 main :: IO ()
 main = parseAndSolve "inputs/day21" part1 part2
