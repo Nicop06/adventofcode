cff2f0b83cb08394e35cfa32c48dda1e5a321f86
Nicolas Porcel <nicolasporcel@google.com>
2022-12-26 23:37:24 +0100

    Day 22 Part 2

--- 2022/day22.hs
+++ 2022/day22.hs
@@ -20,7 +20,11 @@ type Path = [Instruction]
 
 type Coordinates = (Int, Int)
 
+type GridSize = (Int, Int)
+
 type Player = (Coordinates, Direction)
+
+type NextTileFunc = Grid -> Player -> Player
 
 -- Helpers
 
@@ -34,16 +38,16 @@ initialCoordinate grid =
         openTile = fst . head . filter ((==Open) . snd) . map (\c -> (c, grid ! ix2 0 c)) $ [0..(numCols - 1)]
         in (0, openTile)
 
-gridSize :: Grid -> Coordinates
+gridSize :: Grid -> GridSize
 gridSize grid = let [c, r] = listOfShape . extent $ grid in (r, c)
 
 initialPlayer :: Grid -> Player
 initialPlayer grid = (initialCoordinate grid, R)
 
-followInstruction :: Grid -> Instruction -> Player -> Player
-followInstruction _ TurnRight = second turnRight
-followInstruction _ TurnLeft = second turnLeft
-followInstruction grid (NumberOftiles n) = move n grid
+followInstruction :: NextTileFunc -> Grid -> Instruction -> Player -> Player
+followInstruction _ _ TurnRight = second turnRight
+followInstruction _ _ TurnLeft = second turnLeft
+followInstruction nextTileFunc grid (NumberOftiles n) = move n nextTileFunc grid
 
 turnRight :: Direction -> Direction
 turnRight U = R
@@ -63,23 +67,75 @@ nextCoord grid D = first (+1)
 nextCoord grid L = second (subtract 1)
 nextCoord grid R = second (+1)
 
-nextTile :: Grid -> Direction -> Coordinates -> Coordinates
-nextTile grid dir coord = let (x, y) = wrapGrid grid $ nextCoord grid dir coord in case (grid ! ix2 x y) of
-    None -> nextTile grid dir (x, y)
-    otherwise -> (x, y)
+nextTile :: Grid -> Player -> Player
+nextTile grid (coord, dir) = let (x, y) = wrapGrid grid $ nextCoord grid dir coord in case (grid ! ix2 x y) of
+    None -> nextTile grid ((x, y), dir)
+    otherwise -> ((x, y), dir)
 
-move :: Int -> Grid -> Player -> Player
-move 0 _ p = p
-move steps grid (coord, dir) = let (x, y) = nextTile grid dir coord in case (grid ! ix2 x y) of
-    Wall -> (coord, dir)
-    otherwise -> move (steps - 1) grid ((x, y), dir)
+move :: Int -> NextTileFunc -> Grid -> Player -> Player
+move 0 _ _ p = p
+move steps nextTileFunc grid player = let ((x, y), dir') = nextTileFunc grid player in case (grid ! ix2 x y) of
+    Wall -> player
+    otherwise -> move (steps - 1) nextTileFunc grid ((x, y), dir')
 
-followPath :: Grid -> Path -> [Player]
-followPath grid = scanl (flip (followInstruction grid)) (initialPlayer grid)
+followPath :: NextTileFunc -> Grid -> Path -> [Player]
+followPath nextTileFunc grid = scanl (flip (followInstruction nextTileFunc grid)) (initialPlayer grid)
 
-finalPassword :: Grid -> Path -> Int
-finalPassword grid path = let ((x, y), dir) = last $ followPath grid path in
-    1000 * (x + 1) + 4 * (y + 1) + fromEnum dir
+password :: Player -> Int
+password ((x, y), dir) = 1000 * (x + 1) + 4 * (y + 1) + fromEnum dir
+
+-- Cube mapping
+
+{-
+Cube shape:
+   UR
+   F
+  LD
+  B
+
+With U = up, R = right, F = forward, D = down, L = left, B = bottom.
+
+Missing link:
+U -> B
+B -> U
+U -> L
+L -> U
+R -> B
+B -> R
+R -> D
+D -> R
+R -> F
+F -> R
+F -> L
+L -> F
+D -> B
+B -> D
+-}
+
+nextTileCube :: Grid -> Player -> Player
+nextTileCube grid p@((x, y), dir) = let (x', y') = nextCoord grid dir (x, y) in
+    if isOnTile grid (x', y')
+    then ((x', y'), dir)
+    else wrapCube
+        where wrapCube
+                | x == 0 && y >= 50 && y < 100 && dir == U = ((y + 100, 0), R) -- U -> B
+                | x >= 150 && x < 200 && y == 0 && dir == L = ((0, x - 100), D) -- B -> U
+                | x >= 0 && x < 50 && y == 50 && dir == L = ((149 - x, 0), R) -- U -> L
+                | x >= 100 && x < 150 && y == 0 && dir == L = ((149 - x, 50), R) -- L -> U
+                | x == 0 && y >= 100 && y < 150 && dir == U = ((199, y - 100), U) -- R -> B
+                | x == 199 && y >= 0 && y < 50 && dir == D = ((0, y + 100), D) -- B -> R
+                | x >= 0 && x < 50 && y == 149 && dir == R = ((149 - x, 99), L) -- R -> D
+                | x >= 100 && x < 150 && y == 99 && dir == R = ((149 - x, 149), L) -- D -> R
+                | x == 49 && y >= 100 && y < 150 && dir == D = ((y - 50, 99), L) -- R -> F
+                | x >= 50 && x < 100 && y == 99 && dir == R = ((49, x + 50), U) -- F -> R
+                | x >= 50 && x < 100 && y == 50 && dir == L = ((100, x - 50), D) -- F -> L
+                | x == 100 && y >= 0 && y < 50 && dir == U = ((y + 50, 50), R) -- L -> F
+                | x == 149 && y >= 50 && y < 100 && dir == D = ((y + 100, 49), L) -- D -> B
+                | x >= 150 && x < 200 && y == 49 && dir == R = ((149, x - 100), U) -- B -> D
+                | otherwise = error ("Invalid position: " ++ show p)
+
+isOnTile :: Grid -> Coordinates -> Bool
+isOnTile grid (x, y) = let (r, c) = gridSize grid in x >= 0 && x < r && y >= 0 && y < c && (grid ! ix2 x y) /= None
 
 -- Parser
 
@@ -96,11 +152,10 @@ parseInput :: Parser (Grid, Path)
 parseInput = (,) <$> parseGrid <*> parsePath <* eof
 
 part1 :: Parser Int
-part1 = uncurry finalPassword <$> parseInput
+part1 = password . last . uncurry (followPath nextTile) <$> parseInput
 
 part2 :: Parser Int
-part2 = return 0
+part2 = password . last . uncurry (followPath nextTileCube) <$> parseInput
 
 main :: IO ()
 main = parseAndSolve "inputs/day22" part1 part2
---main = parseAndSolve "/tmp/testinput" part1 part2
