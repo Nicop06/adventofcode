b408e1fd2fcbc23dc63e8d8773fa9e194665ea20
Nicolas Porcel <nicolasporcel@google.com>
2023-01-05 18:40:47 +0000

    2015 Day 14 Part 2

--- 2015/day14.hs
+++ 2015/day14.hs
@@ -1,21 +1,24 @@
-import Data.List (groupBy, permutations, sortOn)
-import Data.Map.Strict qualified as M
+import Data.List (transpose)
 import Text.Parsec
 import Text.Parsec.String
 
 -- Data
 
-data Reindeer = Reindeer { name :: String, speed :: Int, flyTime :: Int, restTime :: Int } deriving Show
+data Reindeer = Reindeer {name :: String, speed :: Int, flyTime :: Int, restTime :: Int} deriving (Show)
 
 -- Helpers
 
-totalDistance :: Int -> Reindeer -> Int
-totalDistance raceTime reindeer =
-    let flyAndRestTime = flyTime reindeer + restTime reindeer
-        distanceBetweenRest = flyTime reindeer * speed reindeer
-        in
-            distanceBetweenRest * (raceTime `div` flyAndRestTime) +
-            speed reindeer * min (raceTime `mod` flyAndRestTime) (flyTime reindeer)
+distancePerSecond :: Reindeer -> [Int]
+distancePerSecond r = scanl1 (+) (concat . repeat $ fly ++ rest)
+  where
+    fly = replicate (flyTime r) (speed r)
+    rest = replicate (restTime r) 0
+
+reindeerScore :: Int -> [Reindeer] -> [Int]
+reindeerScore time = map sum . transpose . map computeScore . transpose . map (take time . distancePerSecond)
+    where
+       computeScore dist = let maxDist = maximum dist in
+            map (\d -> if d == maxDist then 1 else 0) dist
 
 -- Parser
 
@@ -35,10 +38,10 @@ parseInput :: IO (Either ParseError [Reindeer])
 parseInput = parseFromFile (parseReindeer `sepEndBy1` newline <* eof) "inputs/day14"
 
 part1 :: [Reindeer] -> IO ()
-part1 = print . maximum . map (totalDistance 2503)
+part1 = print . maximum . map ((!! 2503) . distancePerSecond)
 
-part2 :: String -> IO ()
-part2 = print
+part2 :: [Reindeer] -> IO ()
+part2 = print . maximum . reindeerScore 2503
 
 main :: IO ()
-main = parseInput >>= either print part1
+main = parseInput >>= either print part2
