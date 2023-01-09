69e4843ff92dcf616690f8ec77e206ddf1f7449a
Nicolas Porcel <nicolasporcel06@gmail.com>
2023-01-09 01:03:25 +0000

    2015 Day 20 Part 2

--- 2015/Day20.hs
+++ 2015/Day20.hs
@@ -10,26 +10,25 @@ import Text.Parsec.String
 
 -- Helpers
 
+primes :: [Int]
+primes = 2 : [i | i <- [3 ..], and [i `mod` j /= 0 | j <- takeWhile ((<= i) . (^ 2)) primes]]
+
 numFactors :: Int -> Int -> Int
 numFactors n prime =
   length . takeWhile ((== 0) . (`rem` prime)) $ iterate (`div` prime) n
 
-dividorsAndPrime :: Int -> [Int] -> ([Int], [Int])
-dividorsAndPrime n primes =
-  let
-    primesToConsider = takeWhile ((<= n) . (^2)) primes
-    primeFactors = filter ((> 0) . fst) $ zip (map (numFactors n) primesToConsider) primesToConsider
-   in case primeFactors of
-        [] -> ([1, n], n : primes)
-        l -> (generateDivisors l, primes)
+primesFactors :: Int -> [(Int, Int)]
+primesFactors n =
+  let primesToConsider = takeWhile ((<= n) . (^ 2)) primes
+   in filter ((> 0) . fst) $ zip (map (numFactors n) primesToConsider) primesToConsider
 
 generateDivisors :: [(Int, Int)] -> [Int]
 generateDivisors [] = [1]
 generateDivisors ((c, d) : rs) =
   (*) <$> generateDivisors rs <*> map (d ^) [0 .. c]
 
-nextHousePresents :: ([Int], [Int]) -> ([Int], [Int])
-nextHousePresents (factors, primes) = dividorsAndPrime (last factors + 1) primes
+allDivisors :: Int -> [Int]
+allDivisors = generateDivisors . primesFactors
 
 -- Parser
 
@@ -37,13 +36,11 @@ parseInput :: Parser Int
 parseInput = read <$> many1 digit <* newline <* eof
 
 part1 :: Int -> IO ()
-part1 n = print . (+ 1) . length . takeWhile (< n) $ housePresents
+part1 n = print . (+ 1) . length . takeWhile (< n) $ map housePresents [1 ..]
   where
-    housePresents = map (sum . map (* 10) . fst) $ iterate nextHousePresents ([1], [])
+    housePresents = (* 10) . sum . allDivisors
 
 part2 :: Int -> IO ()
-part2 n = print . (+ 1) . length . takeWhile (< n) $ housePresents
+part2 n = print . (+ 1) . length . takeWhile (< n) $ map housePresents [1..]
   where
-    housePresents = map (sum . map (* 10) . fst) $ iterate nextHousePresents ([1], [])
-
--- part2 n = print . (!! 786240) $ housePresents
+    housePresents h = (* 11) . sum . filter ((>= h) . (* 50)) $ allDivisors h
