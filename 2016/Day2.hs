340b96f70740ee2dc4ab8e013a8d8b315038bcb0
Nicolas Porcel <nicolasporcel06@gmail.com>
2023-04-23 17:27:11 +0100

    2016 Day 2 Part 2

--- 2016/Day2.hs
+++ 2016/Day2.hs
@@ -13,30 +13,28 @@ type Coordinates = (Int, Int)
 data Instruction = U | D | L | R deriving (Read, Show, Eq)
 
 nextCoord :: Instruction -> Coordinates -> Coordinates
-nextCoord R (x, 2) = (x, 2)
 nextCoord R (x, y) = (x, y + 1)
-nextCoord L (x, 0) = (x, 0)
 nextCoord L (x, y) = (x, y - 1)
-nextCoord U (0, y) = (0, y)
 nextCoord U (x, y) = (x - 1, y)
-nextCoord D (2, y) = (2, y)
 nextCoord D (x, y) = (x + 1, y)
 
-nextButton :: Coordinates -> [Instruction] -> Coordinates
-nextButton = foldl (flip nextCoord)
+nextButton :: (Coordinates -> Bool) -> Coordinates -> [Instruction] -> Coordinates
+nextButton allowedCoord = foldl (flip nextCoord')
+    where nextCoord' i c = let c' = nextCoord i c in if allowedCoord c' then c' else c
 
-codeFromInstructions :: [[Instruction]] -> [Coordinates]
-codeFromInstructions = tail . scanl nextButton (1, 1)
-
-buttonFromCoord :: Coordinates -> Int
-buttonFromCoord (x, y) = 3 * x + y + 1
+codeFromInstructions :: (Coordinates -> Bool) -> Coordinates -> [[Instruction]] -> [Coordinates]
+codeFromInstructions allowedCoord start = tail . scanl (nextButton allowedCoord) start
 
 parseInput :: Parser [[Instruction]]
 parseInput = many1 (many1 parseInstruction <* newline)
     where parseInstruction = read . pure <$> oneOf "UDLR"
 
 part1 :: [[Instruction]] -> IO ()
-part1 = putStrLn . concatMap (show . buttonFromCoord) . codeFromInstructions
+part1 = putStrLn . concatMap (show . buttonFromCoord) . codeFromInstructions allowedCoord (1, 1)
+    where buttonFromCoord (x, y) = 3 * x + y + 1
+          allowedCoord (x, y) = (x >= 0) && (y >= 0) && (x < 3) && (y < 3)
 
 part2 :: [[Instruction]] -> IO ()
-part2 = print . head
+part2 = putStrLn . map buttonFromCoord . codeFromInstructions allowedCoord (2, 0)
+    where buttonFromCoord (x, y) = let index = sum (take x [1, 3, 5, 3]) + y - ([2, 1, 0, 1, 2] !! x) in (['1'..'9'] ++ ['A'..'D']) !! index
+          allowedCoord (x, y) = (x + y >= 2) && (x + y <= 6) && (y - x <= 2) && (x - y <= 2) 
