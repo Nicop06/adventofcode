0a02a834c57aec1846fc13857c1fb51b6a87fa23
Nicolas Porcel <nicolasporcel06@gmail.com>
2023-04-23 17:56:28 +0100

    2016 Day 3 Part 2

--- 2016/Day3.hs
+++ 2016/Day3.hs
@@ -7,19 +7,24 @@ where
 
 import Text.Parsec
 import Text.Parsec.String
+import Data.List (transpose)
 
 data Triangle = Triangle Int Int Int deriving (Show, Eq)
 
 isValid :: Triangle -> Bool
 isValid (Triangle x y z) = x + y > z && x + z > y && y + z > x
 
+groupTriangles :: [Int] -> [Triangle]
+groupTriangles (x : y : z : rs) = Triangle x y z : groupTriangles rs
+groupTriangles _ = []
+
 parseInput :: Parser [[Int]]
-parseInput = many1 parseTriangle <* eof
-    where parseTriangle = Triangle <$> parseCoord <*> parseCoord <*> parseCoord <* newline
+parseInput = many1 parseLines <* eof
+    where parseLines = count 3 parseCoord <* newline
           parseCoord = read <$> (spaces *> many1 digit)
 
-part1 :: [Triangle] -> IO ()
-part1 = print . length . filter isValid
+part1 :: [[Int]] -> IO ()
+part1 = print . length . filter isValid . groupTriangles . concat
 
-part2 :: [Triangle] -> IO ()
-part2 = part1
+part2 :: [[Int]] -> IO ()
+part2 = part1 . transpose
