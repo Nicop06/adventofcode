ca158ad34d7b998adff6c42023c638751ef8c37e
Nicolas Porcel <nicolasporcel06@gmail.com>
2023-04-24 22:30:32 +0100

    2016 Day 5 Part 2

--- 2016/Day5.hs
+++ 2016/Day5.hs
@@ -8,24 +8,42 @@ where
 import Crypto.Hash.MD5 (hash)
 import Data.ByteString.Builder (byteStringHex, toLazyByteString)
 import Data.ByteString.Char8 qualified as B
+import Data.Char (ord)
 import Data.List (isPrefixOf)
+import Data.Maybe (catMaybes, fromMaybe, isJust)
 import Text.Parsec
 import Text.Parsec.String
 
-md5Hex :: String -> String
+type Password = [Maybe Char]
+
+type Hash = String
+
+md5Hex :: Hash -> Hash
 md5Hex = B.unpack . B.toStrict . toLazyByteString . byteStringHex . hash . B.pack
 
-generateHashes :: String -> [String]
-generateHashes s = [md5Hex (s ++ show i) | i <- [0 ..]]
+generateHashes :: String -> [Hash]
+generateHashes s = filter ("00000" `isPrefixOf`) [md5Hex (s ++ show i) | i <- [0 ..]]
 
-generatePassword :: String -> String
-generatePassword = map (!! 5) . filter ("00000" `isPrefixOf`) . generateHashes
+isValidPassword :: Password -> Bool
+isValidPassword = all isJust
+
+getIndex :: Hash -> Int
+getIndex s = ord (s !! 5) - ord '0'
+
+nextPassword :: Password -> Hash -> Password
+nextPassword p s = case splitAt (getIndex s) p of
+  (h, c : t) -> h ++ (Just (fromMaybe (s !! 6) c) : t)
+  _ -> p
+
+generatePasswords :: String -> [Password]
+generatePasswords s = scanl nextPassword initialPassword $ generateHashes s
+    where initialPassword = replicate 8 Nothing
 
 parseInput :: Parser String
 parseInput = many1 alphaNum <* newline <* eof
 
 part1 :: String -> IO ()
-part1 = putStrLn . take 8 . generatePassword
+part1 = putStrLn . take 8 . map (!! 5) . generateHashes
 
 part2 :: String -> IO ()
-part2 = print . md5Hex
+part2 = putStrLn . catMaybes . head . filter isValidPassword . generatePasswords
