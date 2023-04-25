0517cec16148309d77e58830c06c60da7ac59b09
Nicolas Porcel <nicolasporcel06@gmail.com>
2023-04-25 21:40:28 +0100

    2016 Day 7 Part 2

--- 2016/Day7.hs
+++ 2016/Day7.hs
@@ -5,38 +5,54 @@ module Day7
   )
 where
 
+import Data.List (isInfixOf, partition)
 import Text.Parsec
 import Text.Parsec.String
 
-data IPPart = Address String | Hypernet String deriving (Show, Eq)
+data IPSequence = Supernet String | Hypernet String deriving (Show, Eq)
 
-type IP = [IPPart]
+type IP = [IPSequence]
 
-isHypernet :: IPPart -> Bool
+isHypernet :: IPSequence -> Bool
 isHypernet (Hypernet _) = True
 isHypernet _ = False
 
-getContent :: IPPart -> String
+getContent :: IPSequence -> String
 getContent (Hypernet s) = s
-getContent (Address s) = s
+getContent (Supernet s) = s
 
 hasAbba :: String -> Bool
 hasAbba (a : rs@(b : c : d : _)) = (a /= b && b == c && a == d) || hasAbba rs
 hasAbba _ = False
 
+listBab :: String -> [String]
+listBab (a : rs@(b : c : _)) =
+  let aba = listBab rs
+   in if a == c && a /= b then [b, a, b] : aba else aba
+listBab _ = []
+
 isTLS :: IP -> Bool
-isTLS parts = not (partsHaveAbba (filter isHypernet parts)) && partsHaveAbba (filter (not . isHypernet) parts)
+isTLS ip =
+  let (hypernet, supernet) = partition isHypernet ip
+   in not (seqsHaveAbba hypernet) && seqsHaveAbba supernet
   where
-    partsHaveAbba = any (hasAbba . getContent)
+    seqsHaveAbba = any (hasAbba . getContent)
+
+isSSL :: IP -> Bool
+isSSL ip =
+  let (hypernet, supernet) = partition isHypernet ip
+      bab = concatMap (listBab . getContent) supernet
+      hasBab s = any (`isInfixOf` s) bab
+   in any (hasBab . getContent) hypernet
 
 parseInput :: Parser [IP]
 parseInput = parseIP `sepEndBy1` newline <* eof
 
 parseIP :: Parser IP
-parseIP = many1 $ (Address <$> many1 alphaNum) <|> (Hypernet <$> between (char '[') (char ']') (many1 alphaNum))
+parseIP = many1 $ (Supernet <$> many1 alphaNum) <|> (Hypernet <$> between (char '[') (char ']') (many1 alphaNum))
 
 part1 :: [IP] -> IO ()
 part1 = print . length . filter isTLS
 
 part2 :: [IP] -> IO ()
-part2 = part1
+part2 = print . length . filter isSSL
