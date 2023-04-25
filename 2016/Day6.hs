ba058036b791ffac47c1faaf217b41f14c7368f1
Nicolas Porcel <nicolasporcel06@gmail.com>
2023-04-25 19:23:32 +0100

    2016 Day 6

---
+++ 2016/Day6.hs
@@ -0,0 +1,22 @@
+module Day6
+  ( parseInput,
+    part1,
+    part2,
+  )
+where
+
+import Text.Parsec
+import Text.Parsec.String
+import Data.List (transpose, sort, sortOn, group)
+
+sortLettersByFrequency :: [Char] -> [Char]
+sortLettersByFrequency = map head . sortOn length . group . sort
+
+parseInput :: Parser [String]
+parseInput = many1 (many1 alphaNum <* newline) <* eof
+
+part1 :: [String] -> IO ()
+part1 = putStrLn . map (last . sortLettersByFrequency) . transpose
+
+part2 :: [String] -> IO ()
+part2 = putStrLn . map (head . sortLettersByFrequency) . transpose
