f80427ee3902cc9b6b41314fa96241c4a6a8bb47
Nicolas Porcel <nicolasporcel06@gmail.com>
2025-12-08 21:41:07 +0000

    Remove repa from day24

--- 2016/Day24.hs
+++ 2016/Day24.hs
@@ -4,10 +4,7 @@ module Day24
   , part2
   ) where
 
-import Control.Monad
-import Data.Array.Repa as R
-import Data.Array.Repa.Repr.Vector as R
-import Prelude as P
+import Data.Array
 import Text.Parsec
 import Text.Parsec.String
 
@@ -17,58 +14,13 @@ data Tile
   | Location Int
   deriving (Eq, Show)
 
-type Grid = Array V DIM2 Tile
-
-type DistanceMatrix = Array V DIM2 Int
-
-isNeighbour :: DIM2 -> DIM2 -> Bool
-isNeighbour ((Z :. x) :. y) ((Z :. x') :. y') =
-  (x == x') && (abs (y - y') == 1) || (y == y') && (abs (x - x') == 1)
-
-neighbours :: DIM2 -> DIM2 -> [DIM2]
-neighbours dim ((Z :. x) :. y) =
-  filter
-    (inShape dim)
-    [ix2 x (y + 1), ix2 x (y - 1), ix2 (x + 1) y, ix2 (x - 1) y]
-
-computeDistanceMatrix :: Grid -> DIM2 -> DistanceMatrix
-computeDistanceMatrix grid start =
-  let dim = extent grid
-      distM = fromListVector dim (replicate (size dim) (size dim))
-   in computeS $
-      go
-        [start]
-        (delay $
-         R.traverse
-           distM
-           id
-           (\f p ->
-              if p == start
-                then 0
-                else f p))
-  where
-    go [] distM = distM
-    go (t:ts) distM =
-      let distM' =
-            R.traverse
-              distM
-              id
-              (\f p ->
-                 if isNeighbour p t && (grid ! p) /= Wall
-                   then min (f p) (f t + 1)
-                   else f p)
-       in go
-            (filter
-               (\p -> distM ! p /= distM' ! p)
-               (neighbours (extent distM) t) P.++
-             ts)
-            distM'
+type Grid = Array (Int, Int) Tile
 
 listToGrid :: [[Tile]] -> Grid
 listToGrid tiles =
   let width = length (head tiles)
       height = length tiles
-   in fromListVector (ix2 height width) $ concat tiles
+   in listArray ((0, 0), (width, height)) $ concat tiles
 
 parseTile :: Parser Tile
 parseTile =
