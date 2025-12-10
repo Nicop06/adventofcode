d11c691fe24aff3e653ccb16bc8ae11fc7ff0ea4
Nicolas Porcel <nicolasporcel06@gmail.com>
2025-12-10 22:30:20 +0000

    2025 Day 10 part 2 naive approach

--- 2025/src/Day10.hs
+++ 2025/src/Day10.hs
@@ -15,12 +15,12 @@ type MachineState = Array Int LightState
 
 newtype Button = Button [Int] deriving (Eq, Show)
 
-newtype Joltage = Joltage [Int] deriving (Eq, Show)
+type Joltage = Array Int Int
 
 data Machine = Machine {getLightState :: MachineState, getButtons :: [Button], getJoltage :: Joltage} deriving (Eq, Show)
 
-makeMachineState :: [LightState] -> MachineState
-makeMachineState lights = let len = length lights in listArray (0, len - 1) lights
+listToArray :: [a] -> Array Int a
+listToArray l = let len = length l in listArray (0, len - 1) l
 
 combinationsOf :: Int -> [a] -> [[a]]
 combinationsOf numElems els = go numElems els
@@ -34,26 +34,35 @@ toggleLight :: LightState -> LightState
 toggleLight On = Off
 toggleLight Off = On
 
-applyButton :: Button -> MachineState -> MachineState
-applyButton (Button idx) machineState = machineState // [(i, toggleLight (machineState ! i)) | i <- idx]
+updateLights :: Button -> MachineState -> MachineState
+updateLights (Button idx) machineState = machineState // [(i, toggleLight (machineState ! i)) | i <- idx]
 
-applyButtons :: Int -> [Button] -> MachineState
-applyButtons n = foldr applyButton (makeMachineState $ replicate n Off)
+updateJoltage :: Button -> Joltage -> Joltage
+updateJoltage (Button idx) joltage = joltage // [(i, 1 + joltage ! i) | i <- idx]
 
 hasAny :: [a] -> Bool
 hasAny [] = False
 hasAny _ = True
 
-canMatchState :: MachineState -> [Button] -> Int -> Bool
-canMatchState m b n = hasAny $ filter (== m) $ map (applyButtons (length m)) (combinationsOf n b)
-
-numButtonsNeeded :: Machine -> Int
-numButtonsNeeded (Machine state buttons _) = go 1
+numButtonsNeeded :: Eq a => (Button -> Array Int a -> Array Int a) -> a -> Array Int a -> [Button] -> Int
+numButtonsNeeded updateFn initVal state buttons = go 1
   where
+    canMatchState :: Int -> Bool
+    canMatchState numButtons =
+      hasAny $
+        filter (== state) $
+          map (foldr updateFn (listToArray $ replicate (length state) initVal)) $
+            combinationsOf numButtons buttons
     go :: Int -> Int
     go n
-      | canMatchState state buttons n = n
+      | canMatchState n = n
       | otherwise = go (n + 1)
+
+numButtonsNeededForLightState :: Machine -> Int
+numButtonsNeededForLightState (Machine lightState buttons _) = numButtonsNeeded updateLights Off lightState buttons
+
+numButtonsNeededForJoltage :: Machine -> Int
+numButtonsNeededForJoltage (Machine _ buttons joltage) = numButtonsNeeded updateJoltage 0 joltage buttons
 
 parseNumber :: Parser Int
 parseNumber = read <$> many1 digit
@@ -62,13 +71,13 @@ parseButton :: Parser Button
 parseButton = Button <$> between (char '(') (char ')') (parseNumber `sepBy1` char ',')
 
 parseJoltage :: Parser Joltage
-parseJoltage = Joltage <$> between (char '{') (char '}') (parseNumber `sepBy` char ',')
+parseJoltage = listToArray <$> between (char '{') (char '}') (parseNumber `sepBy` char ',')
 
 parseLightState :: Parser LightState
 parseLightState = Off <$ char '.' <|> On <$ char '#'
 
 parseMachineState :: Parser MachineState
-parseMachineState = makeMachineState <$> between (char '[') (char ']') (many1 parseLightState)
+parseMachineState = listToArray <$> between (char '[') (char ']') (many1 parseLightState)
 
 parseMachine :: Parser Machine
 parseMachine = Machine <$> parseMachineState <* char ' ' <*> (parseButton `endBy1` char ' ') <*> parseJoltage
@@ -77,7 +86,7 @@ parseInput :: Parser [Machine]
 parseInput = parseMachine `sepEndBy1` newline <* eof
 
 part1 :: [Machine] -> IO ()
-part1 = print . sum . map numButtonsNeeded
+part1 = print . sum . map numButtonsNeededForLightState
 
 part2 :: [Machine] -> IO ()
-part2 = print . const 10
+part2 = print . sum . map numButtonsNeededForJoltage
