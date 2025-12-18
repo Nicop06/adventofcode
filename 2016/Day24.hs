6d9e315fa8a9052e25b7f8a14a6b2fd6f641ffb4
Nicolas Porcel <nicolasporcel06@gmail.com>
2025-12-18 23:39:16 +0000

    2016 Day 24

--- 2016/Day24.hs
+++ 2016/Day24.hs
@@ -5,7 +5,12 @@ module Day24
   )
 where
 
+import Control.Arrow (first, second, (&&&))
 import Data.Array
+import Data.List (permutations)
+import Data.Map as M (Map, fromList, lookup)
+import Data.Maybe (fromMaybe)
+import GHC.Arr
 import Text.Parsec
 import Text.Parsec.String
 
@@ -15,13 +20,60 @@ data Tile
   | Location Int
   deriving (Eq, Show)
 
-type Grid = Array (Int, Int) Tile
+type Coord = (Int, Int)
+
+type Grid = Array Coord Tile
+
+type DistMatrix = Array Coord Int
+
+isLocation :: Tile -> Bool
+isLocation (Location _) = True
+isLocation _ = False
+
+getNeighbours :: (Coord, Coord) -> Coord -> [Coord]
+getNeighbours b c = filter (inRange b) $ [first, second] <*> [(+ 1), flip (-) 1] <*> [c]
+
+distMatrix :: Coord -> Grid -> DistMatrix
+distMatrix start grid = go [start] (amap (const maxBound) grid // [(start, 0)])
+  where
+    go :: [Coord] -> DistMatrix -> DistMatrix
+    go [] m = m
+    go (c : cs) m =
+      let newDist = (m ! c) + 1
+          neighbours = filter ((/= Wall) . (grid !)) . filter ((> newDist) . (m !)) $ getNeighbours (bounds m) c
+       in go (cs ++ neighbours) (m // map (,newDist) neighbours)
+
+distanceToAllLocations :: Grid -> Coord -> Map Coord Int
+distanceToAllLocations grid c = fromList $ filter (isLocation . (grid !) . fst) $ assocs $ distMatrix c grid
+
+allLocations :: Grid -> [Coord]
+allLocations grid = filter (isLocation . (grid !)) $ indices grid
+
+startLocation :: Grid -> Coord
+startLocation grid = head . filter ((== Location 0) . (grid !)) $ indices grid
+
+possibleOrderings :: Grid -> [[Coord]]
+possibleOrderings grid = map (startLocation grid :) (permutations nonStartLocations)
+  where
+    nonStartLocations :: [Coord]
+    nonStartLocations = filter ((/= Location 0) . (grid !)) (allLocations grid)
+
+minDistance :: [[Coord]] -> Grid -> Int
+minDistance orderings grid = minimum $ map totalDistance orderings
+  where
+    allDistanceMappings :: Map Coord (Map Coord Int)
+    allDistanceMappings = fromList $ map (id &&& distanceToAllLocations grid) (allLocations grid)
+
+    totalDistance :: [Coord] -> Int
+    totalDistance [] = 0
+    totalDistance [_] = 0
+    totalDistance (c1 : c2 : cs) = fromMaybe 0 (M.lookup c2 =<< M.lookup c1 allDistanceMappings) + totalDistance (c2 : cs)
 
 listToGrid :: [[Tile]] -> Grid
 listToGrid tiles =
   let width = length (head tiles)
       height = length tiles
-   in listArray ((0, 0), (width, height)) $ concat tiles
+   in listArray ((0, 0), (height - 1, width - 1)) $ concat tiles
 
 parseTile :: Parser Tile
 parseTile =
@@ -33,7 +85,7 @@ parseInput :: Parser Grid
 parseInput = listToGrid <$> (many1 parseTile `sepEndBy1` newline) <* eof
 
 part1 :: Grid -> IO ()
-part1 = print
+part1 grid = print . minDistance (possibleOrderings grid) $ grid
 
 part2 :: Grid -> IO ()
-part2 _ = print 1
+part2 grid = print . minDistance (map (\ord -> ord ++ [startLocation grid]) $ possibleOrderings grid) $ grid
