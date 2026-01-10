a39ee67f512dfca2069666bbf786dd8807db80fd
Nicolas Porcel <nicolasporcel06@gmail.com>
2026-01-10 13:07:14 +0000

    2018 Day 9

--- 2018/src/Day9.hs
+++ 2018/src/Day9.hs
@@ -5,27 +5,33 @@ module Day9
   )
 where
 
-import Data.Map as M (Map, fromListWith)
-import Text.Parsec
+import Data.Sequence
+import Text.Parsec hiding (Empty)
 import Text.Parsec.String
+import Prelude hiding (drop, length, replicate, take)
 
-playMarble :: Int -> [Int] -> (Int, [Int])
+playMarble :: Int -> Seq Int -> (Int, Seq Int)
 playMarble marble circle
   | marble > 0 && (marble `mod` 23) == 0 =
       let i = length circle - 7
-       in (marble + circle !! i, drop (i + 1) circle ++ take i circle)
-  | otherwise = (0, marble : drop 2 circle ++ take 2 circle)
+       in (marble + circle `index` i, drop (i + 1) circle >< take i circle)
+  | otherwise = (0, (marble <| drop 2 circle) >< take 2 circle)
 
 playUntil :: Int -> [Int]
-playUntil lastMarble = go 0 []
+playUntil lastMarble = go 0 Empty
   where
     go marble circle
       | marble > lastMarble = []
       | otherwise = let (score, circle') = playMarble marble circle in score : go (marble + 1) circle'
 
-gameScores :: Int -> Int -> Map Int Int
+gameScores :: Int -> Int -> Seq Int
 gameScores numPlayers lastMarble =
-  M.fromListWith (+) $ zip (cycle [1 .. numPlayers]) (playUntil lastMarble)
+  foldl updateScore (replicate numPlayers 0) (playUntil lastMarble)
+
+updateScore :: Seq Int -> Int -> Seq Int
+updateScore scores score = case viewl scores of
+  EmptyL -> Empty
+  (a :< as) -> as |> (a + score)
 
 parseInput :: Parser (Int, Int)
 parseInput = do
@@ -41,4 +47,4 @@ part1 :: (Int, Int) -> IO ()
 part1 (numPlayers, lastMarble) = print $ maximum (gameScores numPlayers lastMarble)
 
 part2 :: (Int, Int) -> IO ()
-part2 _ = print $ maximum (gameScores 13 7999)
+part2 (numPlayers, lastMarble) = print $ maximum (gameScores numPlayers (lastMarble * 100))
